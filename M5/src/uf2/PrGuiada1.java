56d44a9f99e49f52c1897675d5dab2f14e0d6667
alumne <alumne@alumnepc>
2021-01-21 10:58:06 +0100

    Commit dels canvis de la nova branca

--- M5/src/uf2/PrGuiada1.java
+++ M5/src/uf2/PrGuiada1.java
@@ -1,21 +1,36 @@
 package uf2;
+
 /**** @author alumne
  * */
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class PrGuiada1 {
-	public static void main(String[] args)throws IOException {
+	public static void main(String[] args) throws IOException {
 		File f = new File("fitxer.html");
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
-			bw.write("<html>");bw.newLine();
-			bw.write("<head>");bw.newLine();
-			bw.write("<title>");bw.newLine();
-			bw.write("Projecte inicial");bw.newLine();
-			bw.write("</title>");bw.newLine();
-			bw.write("</head>");bw.newLine();
-			bw.write("<body>");bw.newLine();
-			bw.write("Projecte inicial");bw.newLine();
-			bw.write("</body>");bw.newLine();
-			bw.write("</html>");bw.newLine();
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+			bw.write("<html>");
+			bw.newLine();
+			bw.write("	<head>");
+			bw.newLine();
+			bw.write("		<title>");
+			bw.newLine();
+			bw.write("			Codi de la nova branca");
+			bw.newLine();
+			bw.write("		</title>");
+			bw.newLine();
+			bw.write("	</head>");
+			bw.newLine();
+			bw.write("	<body>");
+			bw.newLine();
+			bw.write("		Projecte inicial");
+			bw.newLine();
+			bw.write("	</body>");
+			bw.newLine();
+			bw.write("</html>");
+			bw.newLine();
 			bw.close();
 		}
 	}
